82d4a31bcba613d5e13459aded64ba88314367d9
Ben Sales <adebsa2401@gmail.com>
2022-11-10 13:13:30 +0100

    tests: Add unit tests for completed status update

--- src/__tests__/statusContentEdit.test.js
+++ src/__tests__/statusContentEdit.test.js
@@ -2,13 +2,13 @@ import { STORAGE_TODO_KEY } from '../data/localStorage.js';
 import Todo from '../models/todo.js';
 
 describe('edit an existing todo task', () => {
-  test('edit the todo task description', () => {
-    // create and add a todo task
-    const todo = new Todo({
-      description: 'Initial description',
-    });
-    todo.add();
+  // create and add a todo task
+  const todo = new Todo({
+    description: 'Initial description',
+  });
+  todo.add();
 
+  test('edit the todo task description', () => {
     // update the todo task description
     todo.edit('Edited description');
 
@@ -17,6 +17,30 @@ describe('edit an existing todo task', () => {
       index: 1,
       description: 'Edited description',
       completed: false,
+    });
+  });
+
+  describe('update task "completed" status', () => {
+    test('toggle "completed" status the first time', () => {
+      todo.toggleCompleted();
+
+      expect(JSON.parse(localStorage.getItem(STORAGE_TODO_KEY))[0]).toEqual({
+        id: 1,
+        index: 1,
+        description: 'Edited description',
+        completed: true,
+      });
+    });
+
+    test('toggle "completed" status the second time', () => {
+      todo.toggleCompleted();
+
+      expect(JSON.parse(localStorage.getItem(STORAGE_TODO_KEY))[0]).toEqual({
+        id: 1,
+        index: 1,
+        description: 'Edited description',
+        completed: false,
+      });
     });
   });
 });
